92a379ca06f4bfb4572c30f0c55ddc18bbacd7fb
ciroDourado <ciro.brz@gmail.com>
2024-05-26 17:31:18 -0300

    refact: altera render de view

--- admin/AdminController.php
+++ admin/AdminController.php
@@ -15,18 +15,12 @@ class AdminController
     
     public function index()
     {
-        ob_start();
         $paginated = $this->listasDeFrequenciaService->paginateListas();
         include __DIR__ . '/partials/index.php';
-
-        return ob_get_clean();
     }
 
     public function create()
     {
-        ob_start();
         include __DIR__ . '/partials/create.php';
-
-        return ob_get_clean();
     }
 }
